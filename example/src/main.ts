c0e2d7e36b3a65e85d9afcdf67651e0a677ebc4b
HongJian <hj@newtons.tech>
2020-10-18 16:59:39 +0800

    build

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import CustomPlugin from '../../src/main'
+import { createApp } from 'vue';
+import App from './App.vue';
+import CustomPlugin from '../../src/main';
 
 const app = createApp(App);
 app.use(CustomPlugin);
-app.mount('#app')
+app.mount('#app');
